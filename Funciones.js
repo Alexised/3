259ae0bd6b5b7b5a74373743c3c75a2e9b45268f
koychi25 <edwinalexis1996@hotmail.com>
2016-03-09 14:46:58 -0500

    Primera Versión

---
+++ Funciones.js
@@ -0,0 +1,15 @@
+function bucle(){
+
+document.write("<center>"+"<h1>" +"tabla de multiplicar"+ "<h1/>"+"</center>")
+
+
+
+for (var i = 1; i <= 10; i++) {
+	document.write("<center>"+"<h2>" +" "+5*i+ "<h2/>"+"</center>")
+	
+	 }
+
+	 	
+
+	}
+
